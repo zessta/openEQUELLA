592fe84f81f650f2470d9c56c0faeaa1062a9983
Chandrashekar Bemagoni <chandrashekar.bemagoni@gmail.com>
2023-05-12 13:47:10 +0530

    Code Refactor

--- Source/Plugins/Core/com.equella.core/src/com/tle/web/remoting/resteasy/CorsInterceptor.java
+++ Source/Plugins/Core/com.equella.core/src/com/tle/web/remoting/resteasy/CorsInterceptor.java
@@ -16,76 +16,77 @@
  * limitations under the License.
  */
 
- package com.tle.web.remoting.resteasy;
+package com.tle.web.remoting.resteasy;
 
- import javax.inject.Inject;
- import javax.inject.Named;
- import javax.servlet.http.HttpServletRequest;
- import javax.ws.rs.core.Context;
- import javax.ws.rs.core.MultivaluedMap;
- import javax.ws.rs.ext.Provider;
- import org.apache.commons.configuration.PropertiesConfiguration;
- import org.jboss.resteasy.annotations.interception.ServerInterceptor;
- import org.jboss.resteasy.core.ServerResponse;
- import org.jboss.resteasy.spi.interception.PostProcessInterceptor;
- 
- @Provider
- @ServerInterceptor
- @SuppressWarnings("nls")
- public class CorsInterceptor implements PostProcessInterceptor {
- 
-   @Inject
-   @Named("cors.policy")
-   private static String cors;
- 
-   @Override
-   public void postProcess(ServerResponse response) {
-     process(response);
-   }
- 
-   @Context static HttpServletRequest servletRequest;
- 
-   public static void runPostProcess(ServerResponse response) {
-     process(response);
-   }
- 
-   private static void process(ServerResponse response) {
-     final MultivaluedMap<String, Object> metadata = response.getMetadata();
-     PropertiesConfiguration pc = null;
-     try {
-       pc =
-           new PropertiesConfiguration(
-               "/home/zadmin/Desktop/new_repo/openEQUELLA/docker/cors.properties");
-     } catch (Exception e) {
-       e.printStackTrace();
-     }
-     String origin = "http://localhost:8080";
-     if (servletRequest != null) {
-       String requestedOrigin = servletRequest.getHeader("Origin");
-       if (requestedOrigin != null && requestedOrigin != "null" && !requestedOrigin.isEmpty()) {
-         String combinedString = pc.getProperty("cors.policy") + "";
-         String[] corsDomains = combinedString.split(",");
-         for (int i = 0; i < corsDomains.length; i++) {
-           String corsDomain = "";
-           if (i == 0) {
-             String temp = corsDomains[i];
-             corsDomain = temp.substring(1, temp.length());
-           } else if (i == corsDomains.length - 1) {
-             String temp = corsDomains[i];
-             corsDomain = temp.substring(0, temp.length() - 1);
-           } else {
-             String temp = corsDomains[i];
-             corsDomain = temp.substring(1, temp.length());
-           }
-           if (requestedOrigin.contains(corsDomain)) {
-             origin = requestedOrigin;
-           }
-         }
-       }
-       System.out.println("origin == " + origin);
-       System.out.println("requestedOrigin == " + requestedOrigin);
-     }
-     metadata.putSingle("Access-Control-Allow-Origin", origin);
-     metadata.putSingle("Access-Control-Expose-Headers", "Location");
-   }
- }
+import javax.servlet.http.HttpServletRequest;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MultivaluedMap;
+import javax.ws.rs.ext.Provider;
+import org.apache.commons.configuration.PropertiesConfiguration;
+import org.jboss.resteasy.annotations.interception.ServerInterceptor;
+import org.jboss.resteasy.core.ServerResponse;
+import org.jboss.resteasy.spi.interception.PostProcessInterceptor;
+
+@Provider
+@ServerInterceptor
+@SuppressWarnings("nls")
+public class CorsInterceptor implements PostProcessInterceptor {
+
+  @Override
+  public void postProcess(ServerResponse response) {
+    process(response);
+  }
+
+  @Context static HttpServletRequest servletRequest;
+
+  public static void runPostProcess(ServerResponse response) {
+    process(response);
+  }
+
+  private static void process(ServerResponse response) {
+    final MultivaluedMap<String, Object> metadata = response.getMetadata();
+    PropertiesConfiguration corsConfiguration = null;
+
+    // Get CORS Properties
+    try {
+      // Use abosulte path in dev mode like
+      // /home/zadmin/codebase/hcl/min-cr/openEQUELLA/docker/cors.properties
+      corsConfiguration = new PropertiesConfiguration("cors.properties");
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+
+    //  Default Origins
+    String origin = corsConfiguration.getProperty("cors.default") + "";
+
+    if (servletRequest != null) {
+
+      // Requested Origin
+      String requestedOrigin = servletRequest.getHeader("Origin");
+
+      // Check is requested Origin is allowed or not
+      if (requestedOrigin != null && requestedOrigin != "null" && !requestedOrigin.isEmpty()) {
+        String combinedString = corsConfiguration.getProperty("cors.allowedOrigins") + "";
+        String[] corsDomains = combinedString.split(",");
+        for (int i = 0; i < corsDomains.length; i++) {
+          String corsDomain = "";
+          if (i == 0) {
+            String temp = corsDomains[i];
+            corsDomain = temp.substring(1, temp.length());
+          } else if (i == corsDomains.length - 1) {
+            String temp = corsDomains[i];
+            corsDomain = temp.substring(0, temp.length() - 1);
+          } else {
+            String temp = corsDomains[i];
+            corsDomain = temp.substring(1, temp.length());
+          }
+          if (requestedOrigin.contains(corsDomain)) {
+            origin = requestedOrigin;
+          }
+        }
+      }
+    }
+    metadata.putSingle("Access-Control-Allow-Origin", origin);
+    metadata.putSingle("Access-Control-Expose-Headers", "Location");
+  }
+}
